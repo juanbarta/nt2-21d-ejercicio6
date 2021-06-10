d985850273db90970fee0f70393a70e52b98dbd9
Juan I. Bartamian <juan.i.bartamian@gmail.com>
2021-06-10 18:31:24 -0300

    minor change

--- src/router.js
+++ src/router.js
@@ -2,14 +2,14 @@ import vue from 'vue'
 import VueRouter from 'vue-router'
 
 vue.use(VueRouter)
-//import Inicio from './componentes/Inicio.vue'
 import Formulario from './componentes/Formulario/index.vue'
 import Http from './componentes/Http.vue'
+import Inicio from './componentes/Inicio.vue'
 
 export const router = new VueRouter({
     mode: 'history',
     routes : [
-        //{path: '/', component: Inicio},
+        {path: '/', component: Inicio},
         {path: '/formulario', component: Formulario},
         {path: '/http', component: Http}
 
